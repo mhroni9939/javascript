ece41b1d35e990f361fefda16c826b331f03f362
Md Mahedi Hasan Roni <128121634+mhroni9939@users.noreply.github.com>
2023-03-25 19:55:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ 
+ 
+ let first_name = prompt(`First Name`);
+ let last_name = prompt(`Your Last Name`); 
+
+ if(last_name == `Haq`) {
+
+    console.log(`My Name Is ${ first_name } ${ last_name } Welcome Haq Vai`);
+
+ }else{
+    console.log(`My Name Is ${first_name} ${last_name} Welcome Vai` )
+
+ }
